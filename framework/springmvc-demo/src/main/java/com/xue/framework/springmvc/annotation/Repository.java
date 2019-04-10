d6236cae3882a162d540f33310247eb3b8987d8e
mingway <571025297@qq.com>
2019-04-10 12:04:17 +0800

    my springmvc framework init

---
+++ framework/springmvc-demo/src/main/java/com/xue/framework/springmvc/annotation/Repository.java
@@ -0,0 +1,15 @@
+package com.xue.framework.springmvc.annotation;
+
+import java.lang.annotation.*;
+
+/**
+ * Created by mingway on Date:2019-04-10 11:55.
+ * 修改记录
+ * 修改后版本:     修改人：  修改日期:     修改内容:
+ */
+@Documented
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Repository {
+	String value();
+}
