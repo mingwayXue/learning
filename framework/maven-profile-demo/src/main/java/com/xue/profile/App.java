4cd9352bcea533ed5d8057bee6604a3af43fdf5b
mingwayXue <571025297@qq.com>
2019-06-12 16:28:00 +0800

    add maven-profile-demo project

---
+++ framework/maven-profile-demo/src/main/java/com/xue/profile/App.java
@@ -0,0 +1,11 @@
+package com.jastar.profile;
+
+import java.io.InputStream;
+
+public class App {
+	public static void main(String[] args) {
+		// This class is no egg 用.
+		System.out.println("Hello,my friends!");
+
+	}
+}
